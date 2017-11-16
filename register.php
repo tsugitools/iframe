35a04c4f7b2f781286b686f876b541eb131577f5
Charles Severance <csev@umich.edu>
2017-11-16 14:31:38 -0500

    Wordsmith

--- register.php
+++ register.php
@@ -4,5 +4,5 @@ $REGISTER_LTI2 = array(
 "name" => "Iframe",
 "FontAwesome" => "fa-window-maximize",
 "short_name" => "Iframe",
-"description" => "This tool allows a link to an Iframe but allows you to view analytics about the use of the iframe."
+"description" => "This tool allows a link to an Iframe and allows you to view analytics about the use of the iframe."
 );
